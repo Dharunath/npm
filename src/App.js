4e3a4af3cbb020b20b03487da693b7c9063de011
dharun <dharunathlete@gmail.com>
2024-01-24 10:09:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hello from './Hello'
+import Tes from './Tes'
+
+export default function App() {
+  return (
+    <div>
+      <Hello/>
+      <Ter/>
+      </div>
+  )
+}
